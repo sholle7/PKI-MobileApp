885286b80765540cff0f8694f00ae3d11adfad8a
Luka Sotra <lukasotra@gmail.com>
2024-02-04 23:13:47 +0100

    Added default layout

---
+++ pki_mobile_app/screens/LoginScreen.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { StyleSheet } from 'react-native';
+
+const LoginScreen = () => {
+  return (
+    <div>LoginScreen</div>
+  )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({
+    loginContainer: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }
+  });
+  
